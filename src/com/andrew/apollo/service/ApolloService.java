4a8aad43206741f5328cf9cfa5d13ad1a8b10b17
maxwen <max.weninger@gmail.com>
2012-10-27 03:08:07 +0200

    fix for NullPointerException
    -fixed coding style
    
    Change-Id: I696922e51933f09925e9d3088386bfcc65fc3245

--- src/com/andrew/apollo/service/ApolloService.java
+++ src/com/andrew/apollo/service/ApolloService.java
@@ -1158,7 +1158,7 @@ public class ApolloService extends Service {
 
     private void setNextTrack() {
         mNextPlayPos = getNextPosition(false);
-        if (mNextPlayPos >= 0) {
+        if (mNextPlayPos >= 0 && mPlayList != null) {
             long id = mPlayList[mNextPlayPos];
             mPlayer.setNextDataSource(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI + "/" + id);
         }
