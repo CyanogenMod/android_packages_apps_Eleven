dcf9649aeffac506ee4e8287bc2af51ed88f129b
linus_lee <llee@cyngn.com>
2014-10-28 11:44:03 -0700

    Eleven: Add a resume check to fix the action bar
    https://cyanogen.atlassian.net/browse/MUSIC-62
    
    Change-Id: I3e8d1aa9b805a4f70e35e9882f2c63b7fa537c01

--- src/com/cyngn/eleven/ui/activities/SlidingPanelActivity.java
+++ src/com/cyngn/eleven/ui/activities/SlidingPanelActivity.java
@@ -165,6 +165,22 @@ public class SlidingPanelActivity extends BaseActivity {
     @Override
     protected void onResume() {
         super.onResume();
+
+        // This is extremely hard to repro but after leaving the app for a long time and coming
+        // back to the app, the action bar can be out of sync with the panel we are looking at -
+        // this will resync the panels
+        if (mTargetNavigatePanel == Panel.None) {
+            switch (getCurrentPanel()) {
+                case Queue:
+                case MusicPlayer:
+                    getActionBar().hide();
+                    break;
+                case Browse:
+                default:
+                    getActionBar().show();
+                    break;
+            }
+        }
     }
 
     /**
