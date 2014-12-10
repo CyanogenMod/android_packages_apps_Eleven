27a1c11b96476fd1882cfedf339a2d09c9be0787
Andrew Neal <andrew.d.neal@gmail.com>
2014-12-10 10:51:42 +0100

    Update Notification background color
    This change uses Palette to update Apollo's Notification background
    color based on the current artwork.
    
    It also removes the timestamp from the Notification.
    
    Change-Id: I6a292839c223bf9d165e9c90724194eae6ca343c

--- Android.mk
+++ Android.mk
@@ -8,6 +8,7 @@ LOCAL_SRC_FILES += $(call all-java-files-under, src)
 
 LOCAL_STATIC_JAVA_LIBRARIES := \
     android-support-v8-renderscript \
+    android-support-v7-palette \
     android-common \
     android-visualizer \
     eleven_support_v4 \
