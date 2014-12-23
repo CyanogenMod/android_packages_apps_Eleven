8652efeebcddbac6e6227360248c348afa897995
linus_lee <llee@cyngn.com>
2014-12-22 19:15:31 -0800

    Eleven: Add some caching logic to BitmapWithColors for perf optimization
    Change-Id: I9fe5e83b9b1b5bb8ca24978436ed371eec2db399

--- src/com/cyanogenmod/eleven/utils/BitmapWithColors.java
+++ src/com/cyanogenmod/eleven/utils/BitmapWithColors.java
@@ -19,23 +19,40 @@ import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.support.v7.graphics.Palette;
 
+import java.util.ArrayList;
+
 public class BitmapWithColors {
+    private static final class BitmapColors {
+        public int mHashCode;
+        public int mVibrantColor;
+        public int mVibrantDarkColor;
+
+        public BitmapColors(int hashCode, int vibrantColor, int vibrantDarkColor) {
+            mHashCode = hashCode;
+            mVibrantColor = vibrantColor;
+            mVibrantDarkColor = vibrantDarkColor;
+        }
+    }
+    private static final int CACHE_SIZE_MAX = 10;
+    private static final int CACHE_SIZE_MIN = 5;
+    private static final ArrayList<BitmapColors> sCachedColors =
+            new ArrayList<BitmapColors>(CACHE_SIZE_MAX);
+
     private Bitmap mBitmap;
-    private int mVibrantColor;
-    private int mVibrantDarkColor;
+    private int mBitmapKey;
+    private BitmapColors mColors;
     private boolean mColorsLoaded = false;
 
-    public BitmapWithColors(Bitmap bitmap) {
+    public BitmapWithColors(Bitmap bitmap, int bitmapKey) {
         mBitmap = bitmap;
-        mVibrantColor = Color.TRANSPARENT;
-        mVibrantDarkColor = Color.TRANSPARENT;
+        mBitmapKey = bitmapKey;
         mColorsLoaded = false;
     }
 
-    public BitmapWithColors(Bitmap bitmap, int vibrantColor, int vibrantDarkColor) {
+    public BitmapWithColors(Bitmap bitmap, int bitmapKey, int vibrantColor, int vibrantDarkColor) {
         mBitmap = bitmap;
-        mVibrantColor = vibrantColor;
-        mVibrantDarkColor = vibrantDarkColor;
+        mBitmapKey = bitmapKey;
+        mColors = new BitmapColors(bitmapKey, vibrantColor, vibrantDarkColor);
         mColorsLoaded = true;
     }
 
@@ -45,47 +62,76 @@ public class BitmapWithColors {
 
     public int getVibrantColor() {
         loadColorsIfNeeded();
-        return mVibrantColor;
+        return mColors.mVibrantColor;
     }
 
     public int getVibrantDarkColor() {
         loadColorsIfNeeded();
-        return mVibrantDarkColor;
+        return mColors.mVibrantDarkColor;
     }
 
-    private void loadColorsIfNeeded() {
-        synchronized (this) {
-            if (mColorsLoaded) {
-                return;
+    private synchronized void loadColorsIfNeeded() {
+        if (mColorsLoaded) {
+            return;
+        }
+
+        BitmapColors colors = getColors(mBitmapKey);
+        if (colors == null) {
+            final Palette p = Palette.generate(mBitmap);
+            int vibrantColor = Color.TRANSPARENT;
+            int vibrantDarkColor = Color.TRANSPARENT;
+
+            if (p != null) {
+                Palette.Swatch swatch = p.getDarkVibrantSwatch();
+                if (swatch != null) {
+                    vibrantDarkColor = swatch.getRgb();
+                }
+                swatch = p.getVibrantSwatch();
+                if (swatch != null) {
+                    vibrantColor = swatch.getRgb();
+                }
             }
-        }
 
-        final Palette p = Palette.generate(mBitmap);
-        int vibrantColor = Color.TRANSPARENT;
-        int vibrantDarkColor = Color.TRANSPARENT;
-
-        if (p != null) {
-            Palette.Swatch swatch = p.getDarkVibrantSwatch();
-            if (swatch != null) {
-                vibrantDarkColor = swatch.getRgb();
+            if (vibrantColor == Color.TRANSPARENT && vibrantDarkColor != Color.TRANSPARENT) {
+                vibrantColor = vibrantDarkColor;
             }
-            swatch = p.getVibrantSwatch();
-            if (swatch != null) {
-                vibrantColor = swatch.getRgb();
+            if (vibrantColor != Color.TRANSPARENT && vibrantDarkColor == Color.TRANSPARENT) {
+                vibrantDarkColor = vibrantColor;
             }
+
+            colors = new BitmapColors(mBitmapKey, vibrantColor, vibrantDarkColor);
+            putColor(colors);
         }
 
-        if (vibrantColor == Color.TRANSPARENT && vibrantDarkColor != Color.TRANSPARENT) {
-            vibrantColor = vibrantDarkColor;
-        }
-        if (vibrantColor != Color.TRANSPARENT && vibrantDarkColor == Color.TRANSPARENT) {
-            vibrantDarkColor = vibrantColor;
-        }
+        mColorsLoaded = true;
+        mColors = colors;
+    }
 
-        synchronized (this) {
-            mColorsLoaded = true;
-            mVibrantColor = vibrantColor;
-            mVibrantDarkColor = vibrantDarkColor;
+
+    private static BitmapColors getColors(int hashCode) {
+        synchronized (sCachedColors) {
+            for (BitmapColors cacheItem : sCachedColors) {
+                if (cacheItem.mHashCode == hashCode) {
+                    return cacheItem;
+                }
+            }
+
+            return null;
+        }
+    }
+
+    private static void putColor(BitmapColors item) {
+        synchronized (sCachedColors) {
+            for (BitmapColors cacheItem : sCachedColors) {
+                if (cacheItem.mHashCode == item.mHashCode) {
+                    return;
+                }
+            }
+
+            sCachedColors.add(item);
+            if (sCachedColors.size() >= CACHE_SIZE_MAX) {
+                sCachedColors.subList(0, sCachedColors.size() - CACHE_SIZE_MIN).clear();
+            }
         }
     }
 }
