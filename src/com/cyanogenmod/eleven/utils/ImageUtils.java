b61672935c5b74e74f15a36657d1887451e413ab
jumoog <kilianvonpflugk@gmail.com>
2015-01-08 03:29:17 -0500

    use last.fm for Non-commercial version of CM
    * Add a new Non-commercial API Key
    * add config switch
    
    !!Please dont use this key for commercial Products!!
    
    Change-Id: I41bc95227576074917576a5d7d0c2545983b4c40

--- src/com/cyanogenmod/eleven/utils/ImageUtils.java
+++ src/com/cyanogenmod/eleven/utils/ImageUtils.java
@@ -18,10 +18,15 @@ package com.cyanogenmod.eleven.utils;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.text.TextUtils;
 import android.widget.ImageView;
 
+import com.cyanogenmod.eleven.R;
 import com.cyanogenmod.eleven.cache.ImageCache;
 import com.cyanogenmod.eleven.cache.ImageWorker;
+import com.cyanogenmod.eleven.Config;
+import com.cyanogenmod.eleven.lastfm.Album;
+import com.cyanogenmod.eleven.lastfm.Artist;
 import com.cyanogenmod.eleven.lastfm.ImageSize;
 import com.cyanogenmod.eleven.lastfm.MusicEntry;
 
@@ -57,21 +62,17 @@ public class ImageUtils {
                                          final String albumName, final ImageWorker.ImageType imageType) {
         switch (imageType) {
             case ARTIST:
-                // Disable last.fm calls - TODO: Find an alternative artwork provider that has
-                // the proper license rights for artwork
-                /*if (!TextUtils.isEmpty(artistName)) {
+                if ((!TextUtils.isEmpty(artistName)) && (context.getResources().getBoolean(R.bool.is_noncommercial))) {
                     if (PreferenceUtils.getInstance(context).downloadMissingArtistImages()) {
                         final Artist artist = Artist.getInfo(context, artistName);
                         if (artist != null) {
                             return getBestImage(artist);
                         }
                     }
-                }*/
+                }
                 break;
             case ALBUM:
-                // Disable last.fm calls - TODO: Find an alternative artwork provider that has
-                // the proper license rights for artwork
-                /*if (!TextUtils.isEmpty(artistName) && !TextUtils.isEmpty(albumName)) {
+                if ((!TextUtils.isEmpty(artistName) && !TextUtils.isEmpty(albumName)) && (context.getResources().getBoolean(R.bool.is_noncommercial))) {
                     if (PreferenceUtils.getInstance(context).downloadMissingArtwork()) {
                         final Artist correction = Artist.getCorrection(context, artistName);
                         if (correction != null) {
@@ -82,7 +83,7 @@ public class ImageUtils {
                             }
                         }
                     }
-                }*/
+                }
                 break;
             default:
                 break;
