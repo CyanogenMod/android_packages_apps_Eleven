48adc1f1521b6a4f205e13a54e053e3ac2fdb74a
Martin Brabham <optedoblivion@cyngn.com>
2015-05-27 15:52:06 -0700

    AudioPreviewActivity: Better external audio previewing
    	- Adds a pop up activity that previews the audio with minimal playback controls
    
    Change-Id: I163e47b3ec91603d788cfdbc8ad0262b33d786e6

---
+++ src/com/cyanogenmod/eleven/ui/activities/preview/PreviewSong.java
@@ -0,0 +1,33 @@
+/*
+* Copyright (C) 2015 The CyanogenMod Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.cyanogenmod.eleven.ui.activities.preview;
+
+import android.net.Uri;
+
+/**
+ * PreviewSong
+ * <pre>
+ *     A POJO representation of a previewable external song
+ * </pre>
+ */
+/* package */ class PreviewSong {
+
+    public Uri URI = null;
+    public String TITLE = null;
+    public String ARTIST = null;
+
+}
