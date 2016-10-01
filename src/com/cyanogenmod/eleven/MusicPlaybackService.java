ab9201c69824c83a2dd561d0dc221cffa78ee664
Steve Kondik <steve@cyngn.com>
2016-09-30 22:15:09 -0700

    eleven: Fix crash when unplugging headphones
     * Don't crash if the handler was never created.
    
    Change-Id: I22aedbf5e912e086db04bbb0dc09d7434dba99b9

--- src/com/cyanogenmod/eleven/MusicPlaybackService.java
+++ src/com/cyanogenmod/eleven/MusicPlaybackService.java
@@ -2502,7 +2502,9 @@ public class MusicPlaybackService extends Service {
     public void pause() {
         if (D) Log.d(TAG, "Pausing playback");
         synchronized (this) {
-            mPlayerHandler.removeMessages(FADEUP);
+            if (mPlayerHandler != null) {
+                mPlayerHandler.removeMessages(FADEUP);
+            }
             if (mIsSupposedToBePlaying) {
                 final Intent intent = new Intent(
                         AudioEffect.ACTION_CLOSE_AUDIO_EFFECT_CONTROL_SESSION);
@@ -2510,7 +2512,9 @@ public class MusicPlaybackService extends Service {
                 intent.putExtra(AudioEffect.EXTRA_PACKAGE_NAME, getPackageName());
                 sendBroadcast(intent);
 
-                mPlayer.pause();
+                if (mPlayer != null) {
+                    mPlayer.pause();
+                }
                 setIsSupposedToBePlaying(false, true);
                 stopShakeDetector(false);
             }
